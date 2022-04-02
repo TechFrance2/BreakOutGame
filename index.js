cef83782317f9bd120d591d41a2426dfd8aa0d6e
Francelli Nakamura <franci.nakamura100@gmail.com>
2022-04-01 19:34:51 -0700

    created one block

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+const grid = document.querySelector(".grid");
+const blockWidth = 100;
+const blockHeight = 20;
 
+//create Block
+class Block {
+  constructor(xAxis, yAxis) {
+    this.bottomLeft = [xAxis, yAxis];
+    this.bottomRight = [xAxis + blockWidth, yAxis];
+    this.topLeft = [xAxis, yAxis + blockHeight];
+    this.topRight = [xAxis + blockWidth, yAxis + blockHeight];
+  }
+}
+
+//all my blocks
+const blocks = [];
+
+//draw my block
+function addBlock() {
+  const block = document.createElement("div");
+  block.classList.add("block");
+  block.style.left = "100px";
+  block.style.bottom = "50px";
+  grid.appendChild(block);
+}
+
+addBlock();
