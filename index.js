47effeb9ca10bdd11e3492a9ae336a93dd98c234
Francelli Nakamura <franci.nakamura100@gmail.com>
2022-04-06 10:38:49 -0700

    ball change directions & increased paddle size

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const grid = document.querySelector(".grid");
 const blockWidth = 100;
-const blockHeight = 15;
+const blockHeight = 20;
+const ballDiameter = 20;
 const boardWidth = 560;
+const boardHeight = 300;
+let timerId;
+let xDirection = 2;
+let yDirection = 2;
 
-const userStart = [205, 10];
+const userStart = [230, 10];
 let currentPosition = userStart;
 
-const ballStart = [230, 40];
+const ballStart = [270, 40];
 let ballCurrentPosition = ballStart;
 
 //create Block
@@ -54,13 +59,18 @@ addBlocks();
 //add user
 const user = document.createElement("div");
 user.classList.add("user");
-drawUser();
 grid.appendChild(user);
+drawUser();
 
 //draw the user
 function drawUser() {
   user.style.left = currentPosition[0] + "px";
   user.style.bottom = currentPosition[1] + "px";
+}
+//draw the ball
+function drawBall() {
+  ball.style.left = ballCurrentPosition[0] + "px";
+  ball.style.bottom = ballCurrentPosition[1] + "px";
 }
 
 //move user
@@ -86,6 +96,51 @@ document.addEventListener("keydown", moveUser);
 //add ball
 const ball = document.createElement("div");
 ball.classList.add("ball");
-ball.style.left = ballCurrentPosition[0] + "px";
-ball.style.bottom = ballCurrentPosition[1] + "px";
+drawBall();
 grid.appendChild(ball);
+
+//move ball
+function moveBall() {
+  ballCurrentPosition[0] += xDirection;
+  ballCurrentPosition[1] += yDirection;
+  drawBall();
+  checkForCollisions();
+}
+
+timerId = setInterval(moveBall, 30);
+
+//check for collisions
+function checkForCollisions() {
+  //check for wall collisions
+  if (
+    ballCurrentPosition[0] >= boardWidth - ballDiameter ||
+    ballCurrentPosition[1] >= boardHeight - ballDiameter ||
+    ballCurrentPosition[0] <= 0
+  ) {
+    changeDirection();
+  }
+
+  // check for game over
+  if (ballCurrentPosition[1] <= 0) {
+    clearInterval(timerId);
+  }
+}
+
+function changeDirection() {
+  if (xDirection === 2 && yDirection == 2) {
+    yDirection = -2;
+    return;
+  }
+  if (xDirection == 2 && yDirection == -2) {
+    xDirection = -2;
+    return;
+  }
+  if (xDirection == -2 && yDirection == -2) {
+    yDirection = 2;
+    return;
+  }
+  if (xDirection === -2 && yDirection == 2) {
+    xDirection = 2;
+    return;
+  }
+}
